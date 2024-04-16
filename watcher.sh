1b1ee968854807c366cbc7e1f6e7c5b2dcb4eb87
A <58280054+absandell@users.noreply.github.com>
2024-04-16 04:13:33 +0000

    Watcher script

--- watcher.sh
+++ watcher.sh
@@ -1,1 +1,18 @@
-#TODO
+namespace="sre"
+deployment="swype-app"
+maxRestarts=3
+
+while true; do
+    numRestarts=$(kubectl get pods -n $namespace | grep $deployment | awk '{print $4}')
+    podName=$(kubectl get pods -n $namespace | grep $deployment | awk '{print $1}')
+    echo "Restarts for pod $podName in namespace $namespace for deployment $deployment = $numRestarts"
+    
+    if (( numRestarts > maxRestarts))
+    then
+        echo "Scaling down due to "$numRestarts" on pod "$podName"..."
+        kubectl scale --replicas=0 deployment/${deployment} -n ${namespace}
+        break
+    fi
+
+    sleep 60
+done
